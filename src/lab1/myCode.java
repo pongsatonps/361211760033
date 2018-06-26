806d6ce5d7d98d11cb859313ef678394d4db4edd
pongsatonps <ab0616412102@gmail.com>
2018-06-26 11:35:29 +0700

    Hello world

---
+++ src/lab1/myCode.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class myCode {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("name:pongsaton suttipan");
+        System.out.println("id: 033");
+        System.out.println("group:ระบบสารสนเทศ");
+        System.out.println("University: มทร.");
+    }//main
+
+
+
+
+
+
+
+
+
+
+}//class
